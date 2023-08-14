8a87c185ea2b3a7529022dff1649e4e1b69cb5ab
Mateo Valencia Sanchez <mateocopa1@hotmail.com>
2023-08-13 21:49:15 -0500

    v2 funciones de las listas

---
+++ listas.py
@@ -0,0 +1,6 @@
+lista_de_comprar = ["salsa de tomate","aceite","arroz","pasta","cereal"]
+a = 0
+lista_de_comprar.append("pastel")
+for a in range (len(lista_de_comprar)):
+    print(f'{lista_de_comprar[a]}')
+    a += 1

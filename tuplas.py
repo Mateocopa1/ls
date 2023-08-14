d9bb7416c5e9be8042f62fa5552becb8fbb42d97
Mateo Valencia Sanchez <mateocopa1@hotmail.com>
2023-08-13 21:31:47 -0500

    actualizacion v1 tuplas

---
+++ tuplas.py
@@ -0,0 +1,6 @@
+peliculas = ("300","El hombre en llamas","El niño de la pijama de rayas")
+horarios = ("17:45","19:20","22:00")
+a = 0
+for a in range(3): 
+ print(f'La película {peliculas[a]} se proyectará a las {horarios[a]}')
+ a += 1
